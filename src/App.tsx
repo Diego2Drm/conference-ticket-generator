2a0db98bcf8176e8a5ffb1edd41c0fb0ace376b1
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-04-24 12:48:27 -0600

    Component Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
+import { Layout } from "./compnents/Layout"
 
 function App() {
 
   return (
-    <>
-        <h1 className="text-Orange-700">Conference Ticket Generator</h1>
-    </>
+    <Layout>
+      <h1 className="text-Orange-700">Conference Ticket Generator</h1>
+
+    </Layout>
   )
 }
 
---
+++ src/compnents/Layout/index.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { PatternLines } from "./Pattern-lines"
+import bgMobile from "../../assets/images/background-mobile.png"
+import PatternSquigglyLineTop from "../../assets/images/pattern-squiggly-line-top.svg"
+import PatternSquigglyLineBottomMobileTablet from "../../assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg"
+
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <section className="relative z-0 overflow-hidden">
+      <div className="absolute w-full h-full z-10 top-0 ">
+        <PatternLines />
+      </div>
+
+      <div className="absolute z-10 top-7 right-0 w-1/2 h-20">
+        <img src={PatternSquigglyLineTop} alt="pattern-squiggly-line-top" className="lg:hidden" />
+      </div>
+
+      <img src={bgMobile} alt="bg-mobile" />
+
+      <div className="absolute z-20 top-0 p-5">
+        {children}
+      </div>
+
+      <div className="absolute z-10 bottom-0 left-0">
+        <img src={PatternSquigglyLineBottomMobileTablet} alt="pattern-squiggly-line-bottom-mobile-tablet" />
+      </div>
+
+    </section>
+  )
+
+}
+
+export { Layout };
